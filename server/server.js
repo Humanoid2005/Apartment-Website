f09918715e14f09554dddc3c9dc55e6a922952ef
Humanoid2005 <sriramsrikanth2005@gmail.com>
2024-07-09 21:15:41 +0530

    Adding basic files

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
+
+app.listen(port,()=>{
+    console.log(`The server is running on http://localhost:${port}`);
+})
