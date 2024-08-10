db86367cd126c455f41e532ea6216d55b2a92d0c
Sriram Srikanth <50945618+Humanoid2005@users.noreply.github.com>
2024-08-10 21:35:06 +0530

    Update useFetch.jsx

--- client/src/components/useFetch.jsx
+++ client/src/components/useFetch.jsx
@@ -31,7 +31,7 @@ const useFetch = (url)=>{
                     setpending(false);
                     seterror(err.message);
                 })
-        },1000);
+        },10000);
 
         return ()=> abortcontrol.abort();
     },[url])
